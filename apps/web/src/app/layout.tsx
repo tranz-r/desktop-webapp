f13e339c1964a5f848f59c57d8f1ed8375f3ffe5
Cloudsty <wi13b051@gmail.com>
2025-11-04 17:12:58 +0000

    feat: added google tag manager

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -93,16 +93,39 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
   return (
     <html lang="en" suppressHydrationWarning>
       {/* <Script src="https://api.tempo.build/proxy-asset?url=https://storage.googleapis.com/tempo-public-assets/error-handling.js" /> */}
       <body className={`${quicksand.variable} font-quicksand`}>
+        {/* Google Tag Manager (noscript) */}
+        {gtmId ? (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
+        ) : null}
         <Providers>
           {children}
           <RouteLoadingOverlay />
           <TempoInit />
           <ChatwootWidget />
         </Providers>
+
+        {/* Google Tag Manager */}
+        {gtmId ? (
+          <Script id="gtm-init" strategy="beforeInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`}
+          </Script>
+        ) : null}
       </body>
     </html>
   );
